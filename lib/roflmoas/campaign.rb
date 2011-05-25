4539cfdce4900551a81f3d3b897da2d376b11a0b
Rodrigo Navarro <rnavarro1@gmail.com>
2011-05-25 18:50:23 -0300

    We don't need to delete keys anymore

--- lib/roflmoas/campaign.rb
+++ lib/roflmoas/campaign.rb
@@ -13,20 +13,20 @@ module RoflmOAS
         xml.Campaign(:action => "add") do
           xml.Overview do
             # Yeah... the attributes must be in the right order =/
-            xml.Id attributes.delete(:id)
-            xml.AdvertiserId attributes.delete(:advertiser_id)
-            xml.Name attributes.delete(:name)
-            xml.AgencyId attributes.delete(:agency_id)
-            xml.Description attributes.delete(:description)
-            xml.CampaignManager attributes.delete(:campaign_manager)
-            xml.ProductId attributes.delete(:product_id)
+            xml.Id attributes[:id]
+            xml.AdvertiserId attributes[:advertiser_id]
+            xml.Name attributes[:name]
+            xml.AgencyId attributes[:agency_id]
+            xml.Description attributes[:description]
+            xml.CampaignManager attributes[:campaign_manager]
+            xml.ProductId attributes[:product_id]
             xml.ExternalUsers do
-              attributes.delete(:external_users).each do |user_id|
+              attributes[:external_users].each do |user_id|
                 xml.UserId user_id
               end
             end if attributes[:external_users]
-            xml.InternalQuickReport attributes.delete(:internal_quick_report)
-            xml.ExternalQuickReport attributes.delete(:external_quick_report)
+            xml.InternalQuickReport attributes[:internal_quick_report]
+            xml.ExternalQuickReport attributes[:external_quick_report]
           end
         end
       end
