6978938bae413167960ec3d4bd0d71068fab9de4
Rodrigo Navarro <rnavarro1@gmail.com>
2011-05-25 12:23:18 -0300

    Fixing client configuration bug

--- lib/roflmoas/client.rb
+++ lib/roflmoas/client.rb
@@ -5,10 +5,10 @@ module RoflmOAS
     attr_accessor :endpoint, :account, :username, :password
 
     def initialize(options = {})
-      options.reverse_merge :endpoint => RoflmOAS.endpoint,
-                            :account  => RoflmOAS.account,
-                            :username => RoflmOAS.username,
-                            :password => RoflmOAS.password
+      options.reverse_merge! :endpoint => RoflmOAS.endpoint,
+                             :account  => RoflmOAS.account,
+                             :username => RoflmOAS.username,
+                             :password => RoflmOAS.password
 
       options.each_pair do |key, value|
         send "#{key}=", value
