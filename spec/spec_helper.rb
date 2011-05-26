b5744a53d2209a21aabaffb09ec29b134e09e85a
Rodrigo Navarro <rnavarro1@gmail.com>
2011-05-26 10:12:06 -0300

    Using VCR erb variables for OAS username, password and account

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,11 @@ Dir[File.join("spec/support/**/*.rb")].each { |f| require f }
 VCR.config do |config|
   config.cassette_library_dir = File.join("spec/fixtures/vcr_cassettes")
   config.stub_with :webmock
-  config.default_cassette_options = { :record => :new_episodes, :match_requests_on => [:uri, :method, :body] }
+  config.default_cassette_options = {
+    :record => :new_episodes,
+    :match_requests_on => [:uri, :method, :body],
+    :erb => { :username => ENV["OAS_USERNAME"], :password => ENV["OAS_PASSWORD"], :account => ENV["OAS_ACCOUNT"] }
+  }
 end
 
 RSpec.configure do |config|
