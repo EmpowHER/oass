4fafe4e84af5d32a087bdd5980d2842b99e34db0
Rodrigo Navarro <rnavarro1@gmail.com>
2011-05-25 22:19:35 -0300

    Don't include headers on cassette match

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ Dir[File.join("spec/support/**/*.rb")].each { |f| require f }
 VCR.config do |config|
   config.cassette_library_dir = File.join("spec/fixtures/vcr_cassettes")
   config.stub_with :webmock
-  config.default_cassette_options = { :record => :new_episodes, :match_requests_on => [:uri, :method, :body, :headers] }
+  config.default_cassette_options = { :record => :new_episodes, :match_requests_on => [:uri, :method, :body] }
 end
 
 RSpec.configure do |config|
