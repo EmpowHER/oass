aad0ce187de340c3ce40553fd9c2f84ce5bbd12f
Rodrigo Navarro <rnavarro1@gmail.com>
2011-05-25 21:10:11 -0300

    Configuring credentials for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'oass'
 require 'vcr'
 
+Dir[File.join("spec/support/**/*.rb")].each { |f| require f }
+
 VCR.config do |config|
   config.cassette_library_dir = File.join("spec/fixtures/vcr_cassettes")
   config.stub_with :webmock
